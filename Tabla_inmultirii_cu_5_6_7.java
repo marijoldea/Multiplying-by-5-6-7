ff06fa901d6cb6a4d1138db5d964e704245dfe9e
marijoldea <mari.joldea@gmail.com>
2017-02-24 14:36:51 +0200

    Add files via upload

---
+++ Tabla_inmultirii_cu_5_6_7.java
@@ -0,0 +1,17 @@
+/**
+ * Created by admin on 2/20/2017.
+ */
+public class Tabla_inmultirii_cu_5_6_7 {
+    public static void main(String[] args) {
+        byte j = 5;
+        for (j = 5; j <= 7; j++) {
+            System.out.print("Tabla inmultirii cu ");
+            System.out.println(j);
+
+       for(int i=0;i<=10;i++) {
+            System.out.println(j+"*"+i+"="+i*j);
+        }
+
+        }
+    }
+}
